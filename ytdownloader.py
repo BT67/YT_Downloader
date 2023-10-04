1113c1e8897f60119411e9ba28855656e4822300
gallo-jeanluc <jlvdgallo@gmail.com>
2023-10-05 07:33:18 +0900

    Changed default resolution. Changed input help message.

--- ytdownloader.py
+++ ytdownloader.py
@@ -2,10 +2,11 @@
 #Created by JGallo 2022/10/18
 #Download youtube videos
 from pytube import YouTube
-import argparse, os
+import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Download YouTube videos')
-parser.add_argument('-i', '--input', help='Video URL')
+parser.add_argument('-i', '--input', help='Video URL, wrapped in quotes \"\"')
 parser.add_argument('-r', '--res', help='Download Resolution: 144,240,260,480,720,1080(default)')
 args = parser.parse_args()
 
@@ -14,7 +15,7 @@ if(args.input == None):
 else:
     url = YouTube(str(args.input))
     if(args.res == None):
-        video = url.streams.filter(res='1080p').first()
+        video = url.streams.filter(res='720p').first()
     elif(args.res != None):
         video = url.streams.filter(res=str(args.res)+'p').first()
 
