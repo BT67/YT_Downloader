61c5e4a22b6d15067a348d07de20be5595c86473
gallo-jeanluc <jlvdgallo@gmail.com>
2023-10-05 07:35:34 +0900

    Reformat

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,26 +1,26 @@
-#ytdownloader.py
-#Created by JGallo 2022/10/18
-#Download youtube videos
+# ytdownloader.py
+# Created by JGallo 2022/10/18
+# Download YouTube videos
+import sys
 from pytube import YouTube
 import argparse
-import sys
 
 parser = argparse.ArgumentParser(description='Download YouTube videos')
 parser.add_argument('-i', '--input', help='Video URL, wrapped in quotes \"\"')
 parser.add_argument('-r', '--res', help='Download Resolution: 144,240,260,480,720,1080(default)')
 args = parser.parse_args()
 
-if(args.input == None):
+if args.input is None:
     parser.print_help(file=None)
 else:
     url = YouTube(str(args.input))
-    if(args.res == None):
+    video = None
+    if args.res is None:
         video = url.streams.filter(res='720p').first()
-    elif(args.res != None):
-        video = url.streams.filter(res=str(args.res)+'p').first()
-
-    if(video == None):
+    elif args.res is not None:
+        video = url.streams.filter(res=str(args.res) + 'p').first()
+    if video is None:
         print('No suitable download stream found')
         sys.exit()
-    elif(video != None):
+    elif video is not None:
         video.download()
